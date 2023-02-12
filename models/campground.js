3753c2cd4fc940cb9f003b1102098b73970cf9d6
essteer <essteer@pm.me>
2023-02-12 12:29:57 +0800

    Section 54 complete
    Completed to end of Image Upload section

--- models/campground.js
+++ models/campground.js
@@ -2,9 +2,20 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+// https://res.cloudinary.com/duiqzld67/image/upload/v1676167826/YelpCamp/dxtact7r7r5llud2yqlp.png
+
+const ImageSchema = new Schema({
+  url: String,
+  filename: String,
+});
+
+ImageSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_200");
+});
+
 const CampgroundSchema = new Schema({
   title: String,
-  images: [{ url: String, filename: String }],
+  images: [ImageSchema],
   price: Number,
   description: String,
   location: String,
